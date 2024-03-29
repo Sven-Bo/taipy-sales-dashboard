9f3e46978f41be5e87a867062715a909fe7b8d71
Sven <contact@pythonandvba.com>
2024-03-29 15:28:03 +0100

    Initial Commit 🎉

---
+++ README.md
@@ -0,0 +1,32 @@
+# Interactive Dashboard with Python – Taipy
+
+Sales Dashboard built-in Python and the Taipy library to visualize Excel data.
+
+## Run the app
+```Powershell
+# vanilla terminal
+python main.py
+
+# quit
+ctrl-c
+```
+
+## Demo
+Sales Dashboard: https://dashboard.taipy.cloud/
+
+
+## 🤝 Get to Know Me & Stay Connected
+- 📺 **YouTube:** [CodingIsFun](https://youtube.com/c/CodingIsFun)
+- 🌐 **Website:** [PythonAndVBA](https://pythonandvba.com)
+- 💬 **Discord:** [Join our Community](https://pythonandvba.com/discord)
+- 💼 **LinkedIn:** [Sven Bosau](https://www.linkedin.com/in/sven-bosau/)
+- 📸 **Instagram:** [Follow me](https://www.instagram.com/sven_bosau/)
+
+## ☕️ Support My Work
+Love my content and want to show appreciation? Why not [buy me a coffee](https://pythonandvba.com/coffee-donation) to fuel my creative engine? Your support means the world to me! 😊
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://pythonandvba.com/coffee-donation)
+
+## 💌 Feedback
+Got some thoughts or suggestions? Don't hesitate to reach out to me at contact@pythonandvba.com. I'd love to hear from you! 💡
+![Logo](https://www.pythonandvba.com/banner-img)
---
+++ main.py
@@ -0,0 +1,150 @@
+import pandas as pd
+from taipy.gui import Gui, notify
+import taipy.gui.builder as tgb
+
+
+data = pd.read_excel(
+    io="supermarket_sales.xlsx",
+    engine="openpyxl",
+    sheet_name="Sales",
+    skiprows=3,
+    usecols="B:R",
+    nrows=1000,
+)
+# Add 'hour' column to dataframe
+data["hour"] = pd.to_datetime(data["Time"], format="%H:%M:%S").dt.hour
+
+
+# Get unique values of each filter
+cities = list(data["City"].unique())
+customer_types = list(data["Customer_type"].unique())
+genders = list(data["Gender"].unique())
+
+
+layout = {
+    "xaxis": {"title": ""},
+    "yaxis": {"title": ""},
+    "margin": {"l": 150},
+}
+
+
+def on_filter(state):
+    if (
+        len(state.cities) == 0
+        or len(state.customer_types) == 0
+        or len(state.genders) == 0
+    ):
+        notify(state, "Error", "No results found. Check the filters.")
+        return
+
+    state.data_filtered, state.sales_by_product_line, state.sales_by_hour = filter(
+        state.cities, state.customer_types, state.genders
+    )
+
+
+def filter(cities, customer_types, genders):
+    data_filtered = data[
+        data["City"].isin(cities)
+        & data["Customer_type"].isin(customer_types)
+        & data["Gender"].isin(genders)
+    ]
+
+    # Sales by product lime
+    sales_by_product_line = (
+        data_filtered[["Product line", "Total"]]
+        .groupby(by=["Product line"])
+        .sum()[["Total"]]
+        .sort_values(by="Total")
+    )
+    sales_by_product_line["Product line"] = sales_by_product_line.index
+
+    # Sales by hour
+    sales_by_hour = (
+        data_filtered[["hour", "Total"]].groupby(by=["hour"]).sum()[["Total"]]
+    )
+    sales_by_hour["hour"] = sales_by_hour.index
+
+    return data_filtered, sales_by_product_line, sales_by_hour
+
+
+with tgb.Page() as page:
+    tgb.toggle(theme=True)
+    tgb.text("📊 Sales Dashboard", class_name="h1 text-center pb2")
+
+    with tgb.layout("1 1 1", class_name="p1"):
+        with tgb.part(class_name="card"):
+            tgb.text("Total Sales:", class_name="h2")
+            tgb.text("US $ {int(data_filtered['Total'].sum())}", class_name="h4")
+
+        with tgb.part(class_name="card"):
+            tgb.text("Average Sales:", class_name="h2")
+            tgb.text("{int(data_filtered['Total'].mean())}", class_name="h4")
+
+        with tgb.part(class_name="card"):
+            tgb.text("Average Rating:", class_name="h2")
+            tgb.text(
+                "{round(data_filtered['Rating'].mean(), 1)}"
+                + "{'⭐' * int(round(round(data_filtered['Rating'].mean(), 1), 0))}",
+                class_name="h4",
+            )
+
+    with tgb.layout("1 1 1", class_name="p1"):
+        tgb.selector(
+            value="{cities}",
+            lov=cities,
+            dropdown=True,
+            multiple=True,
+            label="Select cities",
+            class_name="fullwidth",
+            on_change=on_filter,
+        )
+        tgb.selector(
+            value="{customer_types}",
+            lov=customer_types,
+            dropdown=True,
+            multiple=True,
+            label="Select customer types",
+            class_name="fullwidth",
+            on_change=on_filter,
+        )
+        tgb.selector(
+            value="{genders}",
+            lov=genders,
+            dropdown=True,
+            multiple=True,
+            label="Select genders",
+            class_name="fullwidth",
+            on_change=on_filter,
+        )
+
+    with tgb.layout("1 1"):
+        tgb.chart(
+            "{sales_by_hour}",
+            x="hour",
+            y="Total",
+            type="bar",
+            title="Sales by Hour",
+            layout=layout,
+        )
+        tgb.chart(
+            "{sales_by_product_line}",
+            x="Total",
+            y="Product line",
+            type="bar",
+            orientation="h",
+            layout=layout,
+            title="Sales by Product Line",
+        )
+
+
+if __name__ == "__main__":
+    data_filtered, sales_by_product_line, sales_by_hour = filter(
+        cities, customer_types, genders
+    )
+    Gui(page).run(
+        title="Sales Dashboard",
+        use_reloader=True,
+        debug=True,
+        watermark="",
+        margin="4em",
+    )
